52631e5a909a6abdac8c1c8d1d8bc048e2ab0bf4
chenyz-a <chenyz-a@glodon.com>
2022-01-13 17:38:18 +0800

    添加sqlite存储用户信息

--- addressbookserver/src/main/java/com/example/addressbookserver/service/PeopleManager.java
+++ addressbookserver/src/main/java/com/example/addressbookserver/service/PeopleManager.java
@@ -2,19 +2,20 @@ package com.example.addressbookserver.service;
 
 import com.example.addressbookserver.entity.User;
 
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 public class PeopleManager {
     private static final PeopleManager instance = new PeopleManager();
+    private final List<User> userList;
 
     private PeopleManager() {
         userList = new ArrayList<>();
         User user = new User();
         user.setId(1);
         user.setGender("男");
-        user.setName("仲崇涛");
+        user.setName("仲崇滔");
         user.setTelephone(123123123);
         userList.add(user);
 
@@ -31,34 +32,86 @@ public class PeopleManager {
     }
 
     public List<User> getUsers() {
-        return userList;
+        Connection conn = null;
+        ResultSet resultSet = null;
+        Statement statement = null;
+        List<User> users = new ArrayList<>();
+        try {
+            Class.forName("org.sqlite.JDBC");
+            conn = DriverManager.getConnection("jdbc:sqlite:F:\\addressbook\\addressbook_server\\addressbookserver\\DataBase\\addressbook.sqlite");
+            statement = conn.createStatement();
+            resultSet = statement.executeQuery("SELECT id, name, telephone, gender FROM user");
+            while (resultSet.next()) {
+                User user = new User();
+                user.setId(resultSet.getInt("id"));
+                user.setName(resultSet.getString("name"));
+                user.setGender(resultSet.getString("gender"));
+                user.setTelephone(resultSet.getInt("telephone"));
+                users.add(user);
+            }
+        } catch (ClassNotFoundException | SQLException ex) {
+            System.out.println(ex.getMessage());
+            ex.printStackTrace();
+        }
+
+
+        try {
+            resultSet.close();
+            statement.close();
+            conn.close();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+        return users;
     }
 
     int addUser(User user) {
-        boolean seen = false;
-        Integer best = null;
-        for (User user1 : userList) {
-            Integer id = user1.getId();
-            if (!seen || id.compareTo(best) > 0) {
-                seen = true;
-                best = id;
-            }
+        Connection conn = null;
+        try {
+            Class.forName("org.sqlite.JDBC");
+            conn = DriverManager.getConnection("jdbc:sqlite:F:\\addressbook\\addressbook_server\\addressbookserver\\DataBase\\addressbook.sqlite");
+
+            PreparedStatement preparedStatement = conn.prepareStatement("INSERT INTO user (name, telephone, gender) VALUES (?,?,?)");
+            preparedStatement.setString(1,user.getName());
+            preparedStatement.setInt(2,user.getTelephone());
+            preparedStatement.setString(3,user.getGender());
+            preparedStatement.execute();
+
+        } catch (ClassNotFoundException | SQLException ex) {
+            System.out.println(ex.getMessage());
+            ex.printStackTrace();
         }
-        Integer newId = seen ? best + 1 : 1;
-        user.setId(newId);
-        userList.add(user);
-        return user.getId();
+
+        try {
+            conn.close();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return 1;
     }
+
     public boolean deleteUser(int id) {
-        for (User user : userList)
-        {
-            if (user.getId() == id)
-            {
-                userList.remove(user);
-                return true;
-            }
+
+        Connection conn = null;
+        try {
+            Class.forName("org.sqlite.JDBC");
+            conn = DriverManager.getConnection("jdbc:sqlite:F:\\addressbook\\addressbook_server\\addressbookserver\\DataBase\\addressbook.sqlite");
+
+            PreparedStatement preparedStatement = conn.prepareStatement("DELETE FROM user WHERE id = ?");
+            preparedStatement.setInt(1, id);
+            preparedStatement.execute();
+
+        } catch (ClassNotFoundException | SQLException ex) {
+            System.out.println(ex.getMessage());
+            ex.printStackTrace();
         }
-        return false;
+
+        try {
+            conn.close();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return true;
     }
-    private final List<User> userList;
 }
